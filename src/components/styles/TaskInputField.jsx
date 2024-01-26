d2f1d4bacf78a26ecd25c2a6b8c9e9723dcaa106
Luan Gustavo <gluan1213@gmail.com>
2024-01-26 16:11:06 -0300

    Modificando a responsividade do input

--- src/components/styles/TaskInputField.jsx
+++ src/components/styles/TaskInputField.jsx
@@ -4,10 +4,14 @@ export const TaskInputField = styled.input`
   padding: 10px;
   border: 1px solid #e1dcdc;
   border-radius: 5px;
-  flex: 1;
   font-size: 1em;
+  width: 80%;
 
   &:focus {
     border: 1px solid #426eff;
+  }
+
+  @media (min-width: 481px) {
+    flex: 1;
   }
 `
