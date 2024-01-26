97a17f87ad53bb028b46579c85be5d4dfeb4c7be
Luan Gustavo <gluan1213@gmail.com>
2024-01-26 16:11:28 -0300

    Updates

--- assets/index-uaNJZKgL.js
+++ assets/index-uaNJZKgL.js
@@ -42,11 +42,15 @@ Error generating stack: `+o.message+`
   padding: 10px;
   border: 1px solid #e1dcdc;
   border-radius: 5px;
-  flex: 1;
   font-size: 1em;
+  width: 80%;
 
   &:focus {
     border: 1px solid #426eff;
+  }
+
+  @media (min-width: 481px) {
+    flex: 1;
   }
 `,Sl=Un.button`
   display: flex;
